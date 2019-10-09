f2de2ee63bbe6421770d655b4f7ee1b711c6625e
venkatesh <venkivenki123@gmail.com>
2019-10-09 22:43:42 +0530

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+
+class Header extends Component {
+    render(){
+        return(
+            <Container>
+                <Row>
+                    <Col>1 of 2</Col>
+                    <Col>2 of 2</Col>
+                </Row>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
+        )
+    }
+}
+
+export default Header;
